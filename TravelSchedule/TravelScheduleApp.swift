7e873bec38a23b660dd73cd393861c8b7fcc867b
gubina-irina <banshchikova.i.s@gmail.com>
2026-03-13 15:27:07 +0500

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Irina Gubina on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
